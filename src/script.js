9b8ba8c82124e4f5b9bc9c378054ffcc28271adc
Ilya Panasenko <hottdogg2008@gmail.com>
2014-03-23 16:17:35 +0200

    some code optimizations and bugfixes r, v, shift+a keys now works for all languages

--- src/script.js
+++ src/script.js
@@ -1,70 +1,74 @@
-var keyActions = {
-	'86' : function() {
-		// v - open article in background tab
+(function () {
+	var keyActions = {
+		'86': {
+			srcElement: ['INPUT', false],
+			handler: function () {
+				// v - open article in background tab
 
-		// opened article
-		var inlineFrame = document.querySelector('.inlineFrame');
-		// var selected, but closed article
-		var selectedEntry = document.querySelector('.u0Entry.selectedEntry');
+				// opened article
+				var inlineFrame = document.querySelector('.inlineFrame');
+				// var selected, but closed article
+				var selectedEntry = document.querySelector('.u0Entry.selectedEntry');
 
-		var article = inlineFrame || selectedEntry;
-		if (article) {
-			var url = article.querySelector('a.title').href;
-			chrome.extension.sendMessage({
-				url : url
-			});
+				var article = inlineFrame || selectedEntry;
+				if (article) {
+					var url = article.querySelector('a.title').href;
+					chrome.extension.sendMessage({
+						url: url
+					});
+				}
+			}
+		},
+		'82': {
+			srcElement: ['INPUT', false],
+			handler: function () {
+				// r - reload articles
+
+				document.getElementById('pageActionRefresh').click();
+			}
+		},
+		's65': {
+			srcElement: ['INPUT', false],
+			handler: function () {
+				// shift+a - mark all as read
+
+				var pageActionMarkAsRead = document.getElementById('pageActionMarkAsRead');
+				if (pageActionMarkAsRead && pageActionMarkAsRead.style.display !== 'none') {
+					// mark as read button is present and visible - we can click on it
+					pageActionMarkAsRead.click();
+				}
+			}
 		}
-	},
-	's65' : function() {
-		// shift+a - mark all as read
+	};
 
-		var pageActionMarkAsRead = document.getElementById('pageActionMarkAsRead');
-		if (pageActionMarkAsRead && pageActionMarkAsRead.style.display !== 'none') {
-			// mark as read button is present and visible - we can click on it
-			pageActionMarkAsRead.click();
+	var getPressedKeys = function (e) {
+		var keys = '';
+
+		e.shiftKey && (keys += 's');
+		e.ctrlKey && (keys += 'c');
+		e.metaKey && (keys += 'm');
+		e.altKey && (keys += 'a');
+
+		keys += e.keyCode;
+
+		return keys;
+	};
+
+	document.addEventListener('keydown', function (e) {
+		var keyInfo = keyActions[getPressedKeys(e)];
+		if (!keyInfo) {
+			return;
 		}
-	}
-};
 
-var getPressedKeys = function(e) {
-	var keys = '';
+		var handler = keyInfo.handler,
+			elementInfo = keyInfo.srcElement;
 
-	e.shiftKey && (keys += 's');
-	e.ctrlKey && (keys += 'c');
-	e.metaKey && (keys += 'm');
-	e.altKey && (keys += 'a');
+		if (handler && e.srcElement.tagName === elementInfo[0] === elementInfo[1]) {
+			handler();
 
-	keys += e.keyCode;
-
-	return keys;
-};
-
-document.addEventListener('keydown', function(e) {
-	var pressedKeys = getPressedKeys(e);
-
-	var handler = keyActions[pressedKeys];
-	if (handler) {
-		handler();
-
-		// stop event propagation
-		e.stopPropagation();
-		e.preventDefault();
-	}
-}, false);
-
-// hide categories with no unread items
-// I'll leave here for now, maybe some optimizations later
-// (function hideCategories() {
-// var categories = document.querySelectorAll('#feedlyTabs .tab');
-// var i = 0, l = categories.length, cat, unread;
-// for (; i < l; i += 1) {
-// cat = categories[i];
-// if (/^\d+_tab/.test(cat.id)) {
-// unread = cat.querySelector('.categoryUnreadCount');
-// if (!unread.innerText.trim() || unread.innerText.trim() === '0') {
-// cat.style.display = 'none';
-// }
-// }
-// }
-// setTimeout(hideCategories, 500);
-// }());
+			// stop event propagation
+			e.stopPropagation();
+			e.preventDefault();
+		}
+	}, false);
+}());
