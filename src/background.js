7bfaa9ce59e2f66b55d09babf99a9e4f687c48d9
Ilya Panasenko <hottdogg2008@gmail.com>
2015-02-12 17:15:58 +0200

    feat: notification on extenstion update

--- src/background.js
+++ src/background.js
@@ -1,8 +1,19 @@
-chrome.extension.onMessage.addListener(function (request, sender) {
-	if (request.url) {
-		chrome.tabs.create({
-			url: request.url,
-			active: false
-		}, function (tab) {});
-	}
+chrome.extension.onMessage.addListener(function (request) {
+    if (request.url) {
+        chrome.tabs.create({
+            url: request.url,
+            active: false
+        }, function (tab) {
+        });
+    }
+});
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    chrome.notifications.create('aaa', {
+        iconUrl: 'icons/readly-64.png',
+        type: 'basic',
+        title: 'You now have the latest version of Readly, yay!',
+        message: 'Please reload Feedly page to enable all new great features'
+    }, function () {
+    });
 });
