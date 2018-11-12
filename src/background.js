ea3bcb70b8882f09ed3fcbb02e40da4f872cdf19
Ilya Panasenko <hottdogg2008@gmail.com>
2018-11-12 21:48:57 +0200

    remove new version notification

--- src/background.js
+++ src/background.js
@@ -6,13 +6,3 @@ chrome.extension.onMessage.addListener(function (request) {
     });
   }
 });
-
-chrome.runtime.onInstalled.addListener(function (details) {
-  chrome.notifications.create('aaa', {
-    iconUrl: 'icons/readly-64.png',
-    type: 'basic',
-    title: 'You now have the latest version of Readly, yay!',
-    message: 'Please reload Feedly page to enable all new great features',
-  }, function () {
-  });
-});
