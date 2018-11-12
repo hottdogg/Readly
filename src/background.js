afb6c2a4e90cf05c38e69089196469a50c380e5f
Ilya Panasenko <hottdogg2008@gmail.com>
2018-11-12 21:48:43 +0200

    chore: update indentation

--- src/background.js
+++ src/background.js
@@ -1,19 +1,18 @@
 chrome.extension.onMessage.addListener(function (request) {
-    if (request.url) {
-        chrome.tabs.create({
-            url: request.url,
-            active: false
-        }, function (tab) {
-        });
-    }
+  if (request.url) {
+    chrome.tabs.create({
+      url: request.url,
+      active: false,
+    });
+  }
 });
 
 chrome.runtime.onInstalled.addListener(function (details) {
-    chrome.notifications.create('aaa', {
-        iconUrl: 'icons/readly-64.png',
-        type: 'basic',
-        title: 'You now have the latest version of Readly, yay!',
-        message: 'Please reload Feedly page to enable all new great features'
-    }, function () {
-    });
+  chrome.notifications.create('aaa', {
+    iconUrl: 'icons/readly-64.png',
+    type: 'basic',
+    title: 'You now have the latest version of Readly, yay!',
+    message: 'Please reload Feedly page to enable all new great features',
+  }, function () {
+  });
 });
